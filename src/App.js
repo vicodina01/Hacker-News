b4780fdb5832855278c98d6b935f2026eecf4cac
vicodina01 <mybi@live.com.mx>
2021-07-16 15:33:57 -0500

    button filters & select

--- src/App.js
+++ src/App.js
@@ -5,46 +5,81 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [currentFilter, setCurrentFilter] = useState([]);
+  const [selectedNews, setSelectedNews] = useState('angular');
+
+  let getLocalFavs = tasks.filter(e => e.localFavs === "true")
 
   useEffect(() => {
-    const getTasks = async () =>{
-      const getTasksFromAPI =  await fetchTasks()
-      setTasks(getTasksFromAPI)
+    const getTasks = async (opt) =>{
+      const tasksFromAPI =  await fetchTasks(opt)
+      setTasks(tasksFromAPI)
+      setFilteredTasks(tasksFromAPI);
+      setCurrentFilter('all');
     }
 
-    getTasks()
+    getTasks(selectedNews);
   },[])
 
-  const fetchTasks =  async () => {
-    const res = await fetch("https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0");
+  const fetchTasks =  async (opt) => {
+   
+    const res = await fetch("https://hn.algolia.com/api/v1/search_by_date?query="+ opt.toLowerCase() +"&page=0");
     const data = await res.json()
-
     const hitList = [];
 
     //push localFavs on current data
     data.hits.forEach((hit) => {
-      hit.localFavs = getLocalFavs(hit.objectID);
-      hitList.push(hit)
+      hit.localFavs = getLocalStorageFavs(hit.objectID);
+      if(hit.author !== undefined &&  hit.author !== null && 
+          hit.story_title !== undefined &&  hit.story_title !== null && 
+          hit.story_url !== undefined &&  hit.story_url !== null && 
+          hit.created_at !== undefined && hit.created_at !== null ){
+            // if the title is too long
+            hit.story_title = (hit.story_title.length > 60 ) ? hit.story_title.substring(0, 60) + ' ...' : hit.story_title
+            hitList.push(hit)
+      }
     });
     console.log(hitList)
+    
     return hitList
   }
 
   //Toggle Favs
   const toggleFavs = (id) => {
- 
-    const localFav = getLocalFavs(id);
-    localStorage.setItem('localStorageFavs_'+ id, !localFav);
-
-    setTasks(tasks.map((task) => task.objectID === id ? { ...task, localFavs: !localFav}: task))
+    //local storage can only store strings
+    const localFav = getLocalStorageFavs(id) === "true" ? "false" : "true";
+    localStorage.setItem('localStorageFavs_'+ id, localFav);
+    setTasks(tasks.map((task) => task.objectID === id ? { ...task, localFavs: localFav}: task)) 
+    setFilteredTasks(currentFilter === 'all' ? tasks : getLocalFavs) 
+    console.log(filteredTasks)
   }
 
   //get LocalFavs
-  const getLocalFavs = (id) => {  
+  const getLocalStorageFavs = (id) => {  
     const localFav = localStorage.getItem('localStorageFavs_'+ id)
-    console.log(localFav)
-    return localFav !== undefined && localFav !== null ? localFav : false;
-    
+    return localFav !== undefined && localFav !== null ? localFav : "false";   
+  }
+
+  //filter local favs
+  const showLocalFavs = () =>{  
+    setFilteredTasks(getLocalFavs)
+    setCurrentFilter('favs')
+    console.log("showLocalFavs" + filteredTasks)
+  }
+
+  //filter all tasks
+  const showAllTasks = () => {
+    setFilteredTasks(tasks)
+    setCurrentFilter('all')
+    console.log("howAllTasks" + filteredTasks)
+  }
+
+
+  const selectNews= (selected) =>{
+    console.log("selected" + selected)
+    setSelectedNews(selected);
+    console.log("setSelectedNews" + selectedNews)
   }
 
   return (
@@ -52,7 +87,13 @@ function App() {
       <Header/>
       
       {tasks.length > 0 ?(
-        <Tasks tasks = {tasks} onToggle={toggleFavs}  />
+        <Tasks tasks = {filteredTasks} 
+            showLocalFavs={showLocalFavs}
+            showAllTasks={showAllTasks}
+            currentFilter={currentFilter}
+            onSelect={selectNews}
+            selectedNews={selectedNews}
+            onToggle={toggleFavs}  />
       ) : (
         "No tasks to wshow"
       )}   
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,42 @@
 
 import Task from "./Task"
 
-const Tasks = ({tasks, onToggle}) => {
+const Tasks = ({tasks, onToggle, showLocalFavs, showAllTasks, currentFilter, onSelect, selectedNews}) => {
+
+    const handleSelectNews= (e) =>{
+        onSelect(e.target.value)
+        e.preventDefault();
+    }
 
     return (
-        <>
-          {tasks.map((task) =>(
-              <Task key={task.objectID} 
-                    task={task} 
-                    onToggle={onToggle}></Task>
-          ))}  
+        <> 
+            <div className="button-container">
+                <button type="button" 
+                    className={`button-allTasks ${currentFilter === 'all' ? 'active': ''}`} 
+                    onClick={() => showAllTasks()} >All</button>
+                <button type="button" 
+                    className={`button-localFavs ${currentFilter === 'favs' ? 'active': ''}`} 
+                    onClick={() => showLocalFavs()} >My Favs</button>   
+            </div>
+          
+            <div>
+                <select id="idsSelectedNews" 
+                    onChange={handleSelectNews} 
+                    value={selectedNews}
+                    className="select-news">
+                        <option value="select">Select your news</option>
+                        <option value="Angular">Angular</option>
+                        <option value="ReactJs">ReactJs</option>
+                        <option value="VueJs">VueJs</option>
+               </select>
+               {selectedNews}
+            </div>
+
+            {tasks.map((task) =>(
+                <Task key={task.objectID} 
+                        task={task} 
+                        onToggle={onToggle}></Task>
+            ))}  
         </>
     )
 }
