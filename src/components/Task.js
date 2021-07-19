a931e926a33152d2c3cd1ca4af5a5c29c83321ae
vicodina01 <mybi@live.com.mx>
2021-07-19 09:35:48 -0500

    text decor

--- src/components/Task.js
+++ src/components/Task.js
@@ -15,7 +15,7 @@ const Task = ({task, onToggle}) => {
                 
                 <img src={iconTime} className="iconmonstr-time-2" alt="iconTime" />
                 <p className="task-date">{moment(task.created_at).fromNow()} by {task.author}</p>
-                <a href={task.story_url} target="_blank" rel="noopener noreferrer">
+                <a href={task.story_url} className="task-link" target="_blank" rel="noopener noreferrer">
                     <h3 className="task-title">{task.story_title}</h3>
                 </a>
             </div>
